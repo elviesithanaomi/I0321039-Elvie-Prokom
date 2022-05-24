ad90f9acd8d6529ed5763a42871106c2f880a72e
unknown <sithanaomi01@gmail.com>
2022-05-24 20:49:44 +0700

    mengerjakan exercise 11

---
+++ Exercise-11/Data/Geometri2D.py
@@ -0,0 +1,18 @@
+# Silakan perhatikan perintah pada file 11_Modul&Paket.ipynb sebelum membuka file ini
+import math
+def luaspersegipanjang (p,l):
+    return p*l
+def kelilingpersegipanjang (p,l):
+    return 2*(p+l)
+def luasbujursangkar (s):
+    return s*s
+def kelilingbujursangkar (s):
+    return 4*s
+def luaslingkaran (r):
+    return math.pi*r*r
+def kelilinglingkaran (r):
+    return 2*math.pi*r
+def luassegitiga (a,t):
+    return (a*t)/2
+def kelilingsegitiga (a,b,c):
+    return a+b+c
